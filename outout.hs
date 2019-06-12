80e3cf383ca36758bfc930c91d68689c9d18e10f
Artur Pusch <artur.pusch@mni.thm.de>
2019-06-12 23:52:29 +0200

    alles Funzt-- threadDelay hat nichts mit der Dauer einer Datei zutun es sind zwei Wiedergaben zu sehen-- wir können uns für eine von beiden entscheiden (mögliche Qualitätsunterschiede sind noch zu testen)

--- outout.hs
+++ outout.hs
@@ -9,32 +9,27 @@ import Sound.ProteaAudio
 waitPayback = do
   n <- soundActive
   when  (n > 0) $ do
-    threadDelay 10000000--Delay muss an die Länge der Datei angepasst sein.
+    threadDelay 500000
     waitPayback
------------------
--- /home/pz/Schreibtisch/FunktionalProg/Projekt/test.wav
- -- FilePath ->-- 
---main :: IO ()
+-----------------------------------------------
 main = do
---    args <- getArgs
---    filename <- case args of
---      a : _ -> pure a
---      _ -> fail "usage: proteaaudio-play SAMPLE_FILE_NAME"
-    let filename = "/home/pz/Schreibtisch/FunktionalProg/Projekt/test.wav"
+    let filename = "/home/pz/Schreibtisch/FunktionalProg/Projekt/testWav.wav"
 
-    result <- initAudio 64 44100 512 -- max channels, mixing frequency, mixing buffer size
+--unterhalb die 1024 auf 512, falls die Soundkarte nicht mehr die beste ist;)
+    result <- initAudio 64 44100 1024 -- max channels, mixing frequency, mixing buffer size
 
     unless result $ fail "failed to initialize the audio system"
 
     -- (A) load sample from file
     sampleA <- sampleFromFile filename 1.0 -- volume
-
+    --erste Wiedergabe
     soundPlay sampleA 1 1 0 1 -- left volume, right volume, time difference between left and right, pitch factor for playback
 
     waitPayback
-
+    let filename = "/home/pz/Schreibtisch/FunktionalProg/Projekt/test.wav"
     -- (B) load from memory buffer
     buffer <- SB.readFile filename
+    --zweite Wiedergabe
     sampleB <- case takeExtension filename of
       ".ogg" -> sampleFromMemoryOgg buffer 1.0
       ".wav" -> sampleFromMemoryWav buffer 1.0
