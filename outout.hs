547f1e33e43839a930706e09e115ddb30c786f7c
Artur Pusch <artur.pusch@mni.thm.de>
2019-06-12 20:38:55 +0200

    Add new file

---
+++ outout.hs
@@ -0,0 +1,39 @@
+import Control.Monad
+import System.Environment
+import System.FilePath
+import qualified Data.ByteString as SB
+import Control.Concurrent
+
+import Sound.ProteaAudio
+
+waitPayback = do
+  n <- soundActive
+  when  (n > 0) $ do
+    threadDelay 1000000
+    waitPayback
+
+main = do
+    args <- getArgs
+    filename <- case args of
+      a : _ -> /./test.wav
+      _ -> fail "usage: proteaaudio-play SAMPLE_FILE_NAME"
+
+    result <- initAudio 64 44100 1024 -- max channels, mixing frequency, mixing buffer size
+    unless result $ fail "failed to initialize the audio system"
+
+    -- (A) load sample from file
+    sampleA <- sampleFromFile filename 1.0 -- volume
+
+    soundPlay sampleA 1 1 0 1 -- left volume, right volume, time difference between left and right, pitch factor for playback
+    waitPayback
+
+    -- (B) load from memory buffer
+    buffer <- SB.readFile filename
+    sampleB <- case takeExtension filename of
+      ".ogg" -> sampleFromMemoryOgg buffer 1.0
+      ".wav" -> sampleFromMemoryWav buffer 1.0
+
+    soundPlay sampleB 1 1 0 1 -- left volume, right volume, time difference between left and right, pitch factor for playback
+    waitPayback
+
+    finishAudio
