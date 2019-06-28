1a855f8b83faeabd3de5b49769e77046092ad7a3
Oliver Habersetzer <oliver.habersetzer@mni.thm.de>
2019-06-28 23:02:46 +0200

    Implement base instruments

--- src/Instrument.hs
+++ src/Instrument.hs
@@ -7,16 +7,31 @@ module Instrument (
 
 import Data.Aeson
 import GHC.Generics
+import System.Directory
 import qualified Data.ByteString.Lazy as BS
 
 readInstruments :: FilePath -> IO [Instrument]
 readInstruments path = do
-    bytes <- BS.readFile path
-    let mb = decode bytes :: Maybe (Instruments)
-    return $ instruments $ mbV mb
+    if path == "" then do
+        return baseInstruments
+    else do
+        isValidPath <- doesFileExist path
+        if isValidPath then do 
+            bytes <- BS.readFile path
+            let mb = decode bytes :: Maybe (Instruments)
+            return $ baseInstruments ++ (instruments $ mbV mb)
+        else
+            error $ "The file " ++ path ++ " does not exist"
     where mbV (Just a) = a
           mbV (Nothing) = error $ "Could not parse " ++ path ++ " to instruments"
 
+baseInstruments = [bo si, bo sa, bo sq, bo tr]
+    where
+        bo osc = Instrument osc osc Nothing
+        si = "sine"
+        sa = "saw"
+        sq = "square"
+        tr = "triangle"
 
 data Instrument = Instrument
     {
