bb63e3a343d16d30be4953bf628db575206058ac
Oliver Habersetzer <oliver.habersetzer@mni.thm.de>
2019-06-29 13:20:16 +0200

    Implement keyboard key to musical key

--- src/Live.hs
+++ src/Live.hs
@@ -8,6 +8,13 @@ import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Gdk.EventM
 import System.Glib.UTFString
 import Data.Char
+import Key
+
+safeKeyString str
+        | str == "comma" = ","
+        | str == "period" = "."
+        | str == "minus" = "-"
+        | otherwise = str
 
 live = do
     initGUI
@@ -21,7 +28,15 @@ live = do
 
     window `on` keyPressEvent $ tryEvent $ do
         key <- Graphics.UI.Gtk.Gdk.EventM.eventKeyName
-        liftIO $ putStrLn (glibToString key ++ " pressed ")
+        let keyString = safeKeyString $ glibToString key
+        liftIO $ putStrLn keyString
+        if (length keyString) == 1 then do
+            let c = keyString !! 0
+            let musicalKey = qwertzToKey c 0
+            liftIO $ putStrLn $ [c] ++ " -> " ++ (show musicalKey)
+        else do
+            liftIO $ putStrLn $ "Uninteresting key..."
+
 
     widgetShowAll window
     mainGUI
